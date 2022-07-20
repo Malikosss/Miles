7c9ab639266c3c53c3b9e7c2ebf643914a94f4d2
Malikosss <malikosss@yandex.ru>
2022-07-20 23:43:07 +0300

    модернизация

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 100; // стоимость одного билета
-        int bonus = 20; // количество рублей для получения одной мили
-        int miles = price / bonus; // количество бонусных миль
-
-        System.out.println("количество ваших бонусных миль - " + (miles));
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
